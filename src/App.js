f16a6f3072adcbb1b08208d71343ea69f18bafb4
Nikhil kumar <96740599+12014862@users.noreply.github.com>
2023-01-31 10:59:28 +0530

    counting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Segment,Button,Header  } from "semantic-ui-react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const Inc=()=>{
+    setCount(count+1);
+  }
+  const Dec=()=>{
+    setCount(count-1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+
+    <Segment  style={{ margin: "10% 38%", textAlign: "center", fontSize: "25px",  padding: "3% 1%", borderRadius: "8px" }}>
+    <h1>Count</h1>
+    <h1>{count}</h1>
+
+    <div>
+    
+    <Button  positive onClick={Inc} primary>Increment </Button>
+    <Button negative disabled={count===0} onClick={Dec} secondary>Decrement</Button>
+  </div>
+   
+ 
+   
+      </Segment>
+
+      <Header as='h3' textAlign='center'>
+      Done by:-Nikhil Kumar (12014862)
+    </Header>
+      
+      
+      
+      </>
   );
 }
-
 export default App;
